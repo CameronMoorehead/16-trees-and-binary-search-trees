39d1d5f2d4b7c98e3622d3236bacb2f3c2bb4fee
Cameron Moorehead <hello@cameronmoorehead.com>
2017-12-18 18:36:28 -0800

    callback pattern to lot and dft added

--- lab-cameron/lib/k-ary-tree.js
+++ lab-cameron/lib/k-ary-tree.js
@@ -7,7 +7,7 @@ const KAryTree = function(value) {
   if (typeof value !== 'number') {
     throw new TypeError('value must be a number');
   }
-  
+
   this.value = value;
   this._children = [];
 };
@@ -20,14 +20,17 @@ KAryTree.prototype.appendChild = function(tree) {
   this._children.push(tree);
 };
 
-KAryTree.prototype.levelOrderTraversal = function() {
+KAryTree.prototype.levelOrderTraversal = function(callback = logValue) {
   const queue = new Queue();
   queue.enqueue(this);
 
   while(queue.getLength() > 0) {
     const item = queue.dequeue();
 
-    console.log(`visiting ${item.value}`);
+    const aux = callback(item);
+    if (aux) {
+      return aux;
+    }
 
     for (let child of item._children) {
       queue.enqueue(child);
@@ -35,19 +38,50 @@ KAryTree.prototype.levelOrderTraversal = function() {
   }
 };
 
-KAryTree.prototype.depthFirstTraversal = function() {
+KAryTree.prototype.depthFirstTraversal = function(callback = logValue) {
   const stack = new Stack();
   stack.push(this);
 
   while(stack.getLength() > 0) {
     const item = stack.pop();
 
-    console.log(`visiting ${item.value}`);
+    const callbackReturn = callback(item);
+    if (callbackReturn) {
+      return callbackReturn;
+    }
 
     for (let child of item._children) {
       stack.push(child);
     }
   }
+};
+
+KAryTree.prototype.find = function(value) {
+  let found = null;
+  const findValue = node => {
+    if (node.value === value) {
+      found = node;
+    }
+    return found;
+  };
+  console.log(found);
+
+  return this.levelOrderTraversal(findValue);
+  // return returnValue;
+};
+
+KAryTree.prototype.toString = function() {
+  let str = '';
+  const treeToString = node => {
+    str += `\n ${node.value}`;
+  };
+
+  this.levelOrderTraversal(treeToString);
+  return str;
+};
+
+const logValue = item => {
+  console.log(`visiting ${item.value}`);
 };
 
 const one = new KAryTree(1);
@@ -69,4 +103,5 @@ three.appendChild(seven);
 
 six.appendChild(eight);
 
-one.depthFirstTraversal();
+
+console.log(one.find(3));
