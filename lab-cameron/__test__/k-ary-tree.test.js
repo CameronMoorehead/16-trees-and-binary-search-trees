20d916ae728a9ec4a2a155244a38c90fe3b2e23a
Cameron Moorehead <hello@cameronmoorehead.com>
2017-12-22 17:45:06 -0800

    K-Ary tests now passing

--- lab-cameron/__test__/k-ary-tree.test.js
+++ lab-cameron/__test__/k-ary-tree.test.js
@@ -21,7 +21,70 @@ describe('k-ary-tree.js', () => {
     });
   });
 
-  describe('KAryTree.levelOrderTraversal(<callback>)', () => {
+  // describe('KAryTree.levelOrderTraversal(<callback>)', () => {
+  //
+  // });
 
+  // describe('KAryTree.depthFirstTraversal(<callback>)', () => {
+  //
+  // });
+
+  describe('KAryTree.find(<value>)', () => {
+    test('KAryTree.find(<value>) should return the found Node in the K-ary tree', () => {
+      const KAryTreeToTest = new KAryTree(1);
+      KAryTreeToTest.appendChild(new KAryTree(2));
+      KAryTreeToTest.appendChild(new KAryTree(3));
+      KAryTreeToTest.appendChild(new KAryTree(4));
+
+      expect(KAryTreeToTest.find(1).value).toEqual(1);
+      expect(KAryTreeToTest.find(2).value).toEqual(2);
+      expect(KAryTreeToTest.find(3).value).toEqual(3);
+      expect(KAryTreeToTest.find(4).value).toEqual(4);
+    });
+
+    test('KAryTree.fin(<value>) should respond with null if no such value exists', () => {
+      const KAryTreeToTest = new KAryTree(1);
+      KAryTreeToTest.appendChild(new KAryTree(2));
+      KAryTreeToTest.appendChild(new KAryTree(3));
+      KAryTreeToTest.appendChild(new KAryTree(4));
+
+      expect(KAryTreeToTest.find(100000)).toBeNull();
+    });
+  });
+
+  describe('KAryTree.toString()', () => {
+    test('KAryTree.toString() should return a string of nodes found in level-order', () => {
+      const KAryTreeToTest = new KAryTree(1);
+      KAryTreeToTest.appendChild(new KAryTree(2));
+      KAryTreeToTest.appendChild(new KAryTree(3));
+      KAryTreeToTest.appendChild(new KAryTree(4));
+
+      const expected = '1 \n2 \n3 \n4 \n';
+
+      expect(KAryTreeToTest.toString()).toEqual(expected);
+    });
+  });
+
+  describe('KAryTree.toArray()', () => {
+    test('KAryTree.toArray() should return an array of nodes in depth-first order', () => {
+      const KAryTreeToTest = new KAryTree(1);
+
+      const two = new KAryTree(2);
+      const three = new KAryTree(3);
+      const four = new KAryTree(4);
+
+      const five = new KAryTree(5);
+      const six = new KAryTree(6);
+
+      KAryTreeToTest.appendChild(two);
+      KAryTreeToTest.appendChild(three);
+      KAryTreeToTest.appendChild(four);
+
+      three.appendChild(five);
+      three.appendChild(six);
+
+      const expected = [KAryTreeToTest, four, three, six, five, two];
+      expect(KAryTreeToTest.toArray()).toEqual(expected);
+    });
   });
 });
